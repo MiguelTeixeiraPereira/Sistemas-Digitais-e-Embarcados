f9b2895e7fcfe8dbc577be4fa110f3b945e93661
MiguelTeixeiraPereira <pmig1303@gmail.com>
2025-06-10 13:26:10 -0300

    Add files via upload

---
+++ SISTEMAS DIGITAIS E EMBARCADOS/AULA 15/ATIVIDADE_3/ATIVIDADE_3.ino
@@ -0,0 +1,40 @@
+#define pin_led1 8
+#define pin_led2 9
+#define pin_botao 2
+#define pin_botao2 3
+int conta = 0;
+int aux = 0;
+int auxb = 0;
+
+
+void setup() {
+  pinMode(pin_led1, OUTPUT);
+  pinMode(pin_led2, OUTPUT);
+  pinMode(pin_botao, INPUT);
+  pinMode(pin_botao2, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  bool estado = digitalRead(pin_botao);
+  bool estado2 = digitalRead(pin_botao2);
+   if (estado == 1) {
+    if (aux == 0) {
+      conta++;
+      aux = 1;
+    }
+  } else {
+    aux = 0;
+  }
+    if (estado2 == 1) {
+    if (auxb == 0) {
+      conta--;
+      auxb = 1;
+    }
+  } else {
+    auxb = 0;
+  }
+  Serial.println(conta);
+}
+
+
